38ee7d61a62448f10a9a0e203620779123cbf053
dtechbro <princeakinola05@gmail.com>
2024-05-16 22:07:05 +0100

    day-2 level 2 done

--- Day-2/level-2/index.js
+++ Day-2/level-2/index.js
@@ -12,29 +12,49 @@ console.log(typeof('10'))
 
 
 // Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
-console.log(parseFloat(9.8))
+console.log(Math.round(parseFloat('9.8')))
 
 
 
 // Check if 'on' is found in both python and jargon
+console.log("python, jargon".includes('on'))
+
+
 
 // I hope this course is not full of jargon. Check if jargon is in the sentence.
+let course = 'I hope this course is not full of jargon'
+console.log(course.includes('jargon'))
+
+
 
 // Generate a random number between 0 and 100 inclusively.
+console.log(Math.floor(Math.random() * 100))
+
 
 // Generate a random number between 50 and 100 inclusively.
+console.log(Math.floor(Math.random() * 50 + 50))
+
 
 // Generate a random number between 0 and 255 inclusively.
+console.log(Math.floor(Math.random() * 255))
+
 
 // Access the 'JavaScript' string characters using a random number.
+let word = 'Javascript'
+console.log(word[Math.floor(Math.random() * word.length)])
+
 
 // Use console.log() and escape characters to print the following pattern.
-
 // 1 1 1 1 1
 // 2 1 2 4 8
 // 3 1 3 9 27
 // 4 1 4 16 64
 // 5 1 5 25 125
+console.log(
+    "1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125"
+)
 
 
 // Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let phrase = 'You cannot end a sentence with because because because is a conjunction'
+console.log(phrase.substr(0, 31) + phrase.substr(55))
