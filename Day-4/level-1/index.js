95025d46944af33ddd4f0540424c8f31cb12e605
Muhammad-Nabeel <princeakinola05@gmail.com>
2024-05-31 17:42:03 +0100

    WIP

--- Day-4/level-1/index.js
+++ Day-4/level-1/index.js
@@ -1,1 +1,2 @@
 // Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+let age = prompt("Enter your age:")
