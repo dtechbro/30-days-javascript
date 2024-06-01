868e99baec0a1842ab8a263fa410edbf46b97172
dtechbro <princeakinola05@gmail.com>
2024-06-01 14:26:33 +0100

    learning

--- Day-4/level-1/index.js
+++ Day-4/level-1/index.js
@@ -1,4 +1,12 @@
 // Get user input using prompt(“Enter your age:”). If user is 18 or older, give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 let age = prompt("Enter your age:")
-let year = new Date().getFullYear()
-let 
+if (age >=18 ) {
+    console.log(`You are old enough to drive!`)
+} else {
+    console.log(`Wait for ${Number(18 - age)} to learn to drive`)
+}
+
+
+// age >= 18
+//     ? console.log(`You are old enough to drive!`)
+//     : console.log(`Wait for ${Number(18 - age)} years to turn 18 and start driving`)
