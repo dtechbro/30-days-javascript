b8c1a61601ee25253a233749dbc3674dbd5e478e
dtechbro <princeakinola05@gmail.com>
2024-06-11 16:11:23 +0100

    learning

--- Day-5/level-2/index.js
+++ Day-5/level-2/index.js
@@ -1,14 +1,17 @@
 // Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
 
+
+
+
 // First remove all the punctuations and change the string to array and count the number of words in the array
+let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+let words = text.replace(/[.,]/g, '').split(' ')
+console.log(words)
+console.log(words.length)
 
-// let text =
-// 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
-// console.log(words)
-// console.log(words.length)
-// ["I", "love", "teaching", "and", "empowering", "people", "I", "teach", "HTML", "CSS", "JS", "React", "Python"]
 
-// 13
+
+
 // In the following shopping cart add, remove, edit items
 
 // const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
@@ -17,6 +20,7 @@
 // remove 'Honey' if you are allergic to honey
 // modify Tea to 'Green Tea'
 // In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
 
 // In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
 
