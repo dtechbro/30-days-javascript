ca05019b87ca174c5778e9d276c97242c9b52f63
dtechbro <princeakinola05@gmail.com>
2024-06-12 12:34:04 +0100

    learning

--- Day-5/level-2/index.js
+++ Day-5/level-2/index.js
@@ -13,9 +13,9 @@ console.log(words.length)
 
 
 // In the following shopping cart add, remove, edit items
-
-// const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
 // add 'Meat' in the beginning of your shopping cart if it has not been already added
+console.log(shoppingCart.unshift('Meat'))
 // add Sugar at the end of you shopping cart if it has not been already added
 // remove 'Honey' if you are allergic to honey
 // modify Tea to 'Green Tea'
