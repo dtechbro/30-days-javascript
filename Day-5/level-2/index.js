beec2fa69c1a07cbaf4703d40d3dc3f6885a8fa8
dtechbro <princeakinola05@gmail.com>
2024-06-15 22:51:46 +0100

    learning

--- Day-5/level-2/index.js
+++ Day-5/level-2/index.js
@@ -1,3 +1,5 @@
+import { countries } from "..countries.js";
+
 // Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
 
 
