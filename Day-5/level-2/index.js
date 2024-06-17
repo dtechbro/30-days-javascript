2ea159579791e713c3c17db1d190c204c264116d
dtechbro <princeakinola05@gmail.com>
2024-06-17 23:07:45 +0100

    learning

--- Day-5/level-2/index.js
+++ Day-5/level-2/index.js
@@ -1,4 +1,4 @@
-import { countries } from "..countries.js";
+// import { countries } from "..countries.js";
 
 // Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
 
@@ -39,9 +39,7 @@ console.log(shoppingCart)
 // In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
 
 // Concatenate the following two variables and store it in a fullStack variable.
-
-// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-// const backEnd = ['Node','Express', 'MongoDB']
-
-// console.log(fullStack)
-// ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd)
+console.log(fullStack)
