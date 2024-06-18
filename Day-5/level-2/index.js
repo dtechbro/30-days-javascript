09dbe0accda9fc01f4eedddbf1e70717a9c0e43d
dtechbro <princeakinola05@gmail.com>
2024-06-18 21:18:33 +0100

    learn

--- Day-5/level-2/index.js
+++ Day-5/level-2/index.js
@@ -34,7 +34,12 @@ shoppingCart[3] = 'Green Tea'
 console.log(shoppingCart)
 
 // In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
-
+if (countries.includes('Ethopia')) {
+    console.log('ETHOPIA')
+} else {
+    countries.push('Ethiopia')
+    console.log(countries)
+}
 
 // In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
 
