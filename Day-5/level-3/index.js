51e1794ffdca168716a1dac0b2f5d92c970b0253
dtechbro <princeakinola05@gmail.com>
2024-06-22 22:59:27 +0100

    array

--- Day-5/level-3/index.js
+++ Day-5/level-3/index.js
@@ -1,9 +1,25 @@
 // The following is an array of 10 students ages:
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
     // Sort the array and find the min and max age
-    console.log(Range(ages))
-    
+    ages.sort()
+    console.log(ages)
+    let minAge = ages[0]
+    let maxAge = ages[ages.length - 1]
+    console.log("Min Age: ", minAge)
+    console.log("Max Age: ", maxAge)
+
+
     // Find the median age(one middle item or two middle items divided by two)
+    let medianAges;
+    if (ages.length % 2 === 0) {
+        let midIndex = ages.length / 2
+        medianAges = (ages[midIndex - 1] + ages[midIndex]) / 2
+    } else {
+        medianAges = ages[Math.floor(ages.length / 2)]
+    }
+    console.log("Median Age: ", medianAges)
+
+
     // Find the average age(all items divided by number of items)
     // Find the range of the ages(max minus min)
     // Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
